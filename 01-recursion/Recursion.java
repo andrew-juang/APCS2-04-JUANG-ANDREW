ef0c3d1af695a6c84a538edf589ba937d6e8f385
Andrew <andrewjuang01@gmail.com>
2021-02-06 20:30:06 -0500

    Implement countNoDoubleLetterWords

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -1,13 +1,23 @@
 public class Recursion {
-    /*
-    *@param s any string
-    *@return a string that is the reversed version of s, do NOT use built in methods to do so, use recursion.
-    */
     public static String reverse(String s){
         if(s.length()<=1){
             return s;
         } else {
             return s.charAt(s.length()-1)+reverse(s.substring(0,s.length()-1));
         }
+    }
+
+    public static long countNoDoubleLetterWords(int length,String word){
+        long ans = 0;
+        if(length==0){
+            return 1;
+        } else {
+            for(int i=0;i<26;i++){
+                if(word.length()==0||(char)('a'+i)!=word.charAt(word.length()-1)){
+                    ans += countNoDoubleLetterWords(length-1,word+(char)('a'+i));
+                }
+            }
+        }
+        return ans;
     }
 }
--- 01-recursion/Tester.java
+++ 01-recursion/Tester.java
@@ -4,5 +4,10 @@ public class Tester {
         System.out.println(Recursion.reverse("o"));
         System.out.println(Recursion.reverse(""));
         System.out.println(Recursion.reverse("12345"));
+        System.out.println(Recursion.countNoDoubleLetterWords(0,"")); //1
+        System.out.println(Recursion.countNoDoubleLetterWords(1,"")); //26
+        System.out.println(Recursion.countNoDoubleLetterWords(2,"")); //650
+        System.out.println(Recursion.countNoDoubleLetterWords(3,"")); //16250
+        System.out.println(Recursion.countNoDoubleLetterWords(4,"")); //406250
     }
 }
