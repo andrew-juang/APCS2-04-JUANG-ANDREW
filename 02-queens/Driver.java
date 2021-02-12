ec09fff24a3cd57d4b5ebd605acb897d7e7f46f2
Andrew <andrewjuang01@gmail.com>
2021-02-11 19:57:52 -0500

    Implement Constructor and toString method

--- 02-queens/Driver.java
+++ 02-queens/Driver.java
@@ -1,3 +1,6 @@
 public class Driver {
-
+    public static void main(String[]args){
+        QueenBoard q = new QueenBoard(3);
+        System.out.println(q.toString());
+    }
 }
--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -1,3 +1,44 @@
 public class QueenBoard {
-    
+    private int[][]board;
+
+    private boolean addQueen(int r, int c){
+        return true;
+    }
+
+    private void removeQueen(int r, int c){
+
+    }
+
+    public QueenBoard(int size){
+        board = new int[size][size];
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                board[i][j]=0;
+            }
+        }
+    }
+
+    public String toString(){
+        String output = "";
+        for(int i=0;i<board.length;i++){
+            String curr = "";
+            for(int j=0;j<board.length;j++){
+                if(board[i][j]!=-1){
+                    curr += "_";
+                } else {
+                    curr += "Q";
+                }
+            }
+            output = output + curr + "\n";
+        }
+        return output;
+    }
+
+    public boolean solve(){
+        return true;
+    }
+
+    public int countSolutions(){
+        return 0;
+    }
 }
