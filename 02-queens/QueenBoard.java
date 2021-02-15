d43f90d0649326e5bbeb1bf10390604e6f05ddd4
Andrew <andrewjuang01@gmail.com>
2021-02-15 15:03:09 -0500

    Removed unnecessary loops

--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -2,33 +2,26 @@ import java.util.*;
 public class QueenBoard {
     private int[][]board;
 
-    private boolean addQueen(int r, int c){
+    public boolean addQueen(int r, int c){
         int i,j;
         boolean output = true;
         if(board[r][c]>0){
             output=false;
         }
-        for(i=0;i<c;i++){
-            if(board[r][i]==-1){
-                output = false;
-            }
-        }
-        for(i=r,j=c;i>=0&&j>=0;i--,j--){
-            if(board[i][j]==-1){
-                output = false;
-            }
-        }
-        for(i=r,j=c;j>=0&&i<board.length;i++, j--){
-            if(board[i][j]==-1){
-                output = false;
-            }
-        }
         if(output){
             board[r][c]=-1;
-            for(i=0;i<c;i++){
-                board[r][i]++;
+            for(i=0;i<board.length;i++){
+                if(board[r][i]!=-1){
+                    board[r][i]++;
+                }
             }
             for(i=r-1,j=c-1;i>=0&&j>=0;i--,j--){
+                board[i][j]++;
+            }
+            for(i=r-1,j=c+1;i>=0&&j<board.length;i--,j++){
+                board[i][j]++;
+            }
+            for(i=r+1,j=c+1;i<board.length&&j<board.length;i++,j++){
                 board[i][j]++;
             }
             for(i=r+1,j=c-1;j>=0&&i<board.length;i++, j--){
@@ -39,13 +32,21 @@ public class QueenBoard {
         return output;
     }
 
-    private void removeQueen(int r, int c){
+    public void removeQueen(int r, int c){
         int i,j;
         board[r][c]=0;
-        for(i=0;i<c;i++){
-            board[r][i]--;
+        for(i=0;i<board.length;i++){
+            if(i!=c){
+                board[r][i]--;
+            }
         }
         for(i=r-1,j=c-1;i>=0&&j>=0;i--,j--){
+            board[i][j]--;
+        }
+        for(i=r-1,j=c+1;i>=0&&j<board.length;i--,j++){
+            board[i][j]--;
+        }
+        for(i=r+1,j=c+1;i<board.length&&j<board.length;i++,j++){
             board[i][j]--;
         }
         for(i=r+1,j=c-1;j>=0&&i<board.length;i++, j--){
