12fd979f88d183215405d0ee54be42aba72971ea
Andrew <andrewjuang01@gmail.com>
2021-03-01 23:55:40 -0500

    Added wrapper method

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -3,10 +3,26 @@ import java.io.*;
 
 public class MazeGenerator {
     public static void generate(char[][]maze,int rows,int cols,int cr,int cc) {
+        maze[cr][cc] = 'S';
+        generate(maze,cr,cc);
+        boolean bool = true;
+        for(int i=maze.length-1;i>=0&&bool;i--){
+            for(int j=maze[0].length-1;j>=0&&bool;j--){
+                if(maze[i][j]==' '){
+                    maze[i][j] = 'E';
+                    bool = false;
+                }
+            }
+        }
+    }
+
+    public static void generate(char[][]maze,int cr,int cc){
         String[] letters = {"N","S","E","W"};
         List<String> dir = Arrays.asList(letters);
         Collections.shuffle(dir);
-        maze[cr][cc]=' ';
+        if(maze[cr][cc]!='S'){
+            maze[cr][cc]=' ';
+        }
         for(int i=0;i<4;i++){
             int nr=cr, nc=cc;
             if(dir.get(i)=="N"){
@@ -22,7 +38,7 @@ public class MazeGenerator {
                 nc -= 1;
             }
             if(nr>0&&nr<maze.length-1&&nc>0&&nc<maze[0].length-1&&count_neb(maze,nr,nc)<2){
-                generate(maze,rows,cols,nr,nc);
+                generate(maze,nr,nc);
             }
         }
     }
