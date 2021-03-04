1ef8ad631971f1bbab0dde253411f5272f50a1e1
Andrew <andrewjuang01@gmail.com>
2021-03-04 13:10:32 -0500

    Initial Commit

---
+++ 04-usaco/USACO.java
@@ -0,0 +1,12 @@
+public class USACO {
+    public static void main (String[]args) {
+
+    }
+    public int bronze (String filename) {
+
+    }
+
+    public int silver (String filename) {
+        
+    }
+}
