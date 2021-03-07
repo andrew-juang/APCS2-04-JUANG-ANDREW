46b23b85e52ea1e3d8ac90edd77e6bddd1ad3180
Andrew <andrewjuang01@gmail.com>
2021-03-06 22:49:38 -0500

    Fixed throws

--- 04-usaco/Driver.java
+++ 04-usaco/Driver.java
@@ -2,12 +2,19 @@ import java.util.*;
 import java.io.*;
 
 public class Driver {
-    public static void main (String[]args) {
-        try {
-            int ans = USACO.bronze("lake1.input");
-            System.out.println(ans);
-        } catch (FileNotFoundException e){
-            System.out.println("File not found.");
-        }
+    public static void main (String[]args) throws FileNotFoundException {
+        int ans = USACO.bronze("lake1.input");
+        int ans2 = USACO.bronze("lake2.input");
+        int ans3 = USACO.bronze("lake3.input");
+        System.out.println(ans);
+        System.out.println(ans2);
+        System.out.println(ans3);
+
+        long ans4 = USACO.silver("cowPath1.input");
+        long ans5 = USACO.silver("cowPath2.input");
+        long ans6 = USACO.silver("cowPath3.input");
+        System.out.println(ans4);
+        System.out.println(ans5);
+        System.out.println(ans6);
     }
 }
