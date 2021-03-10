78546a1587cbf2e8b0a73fc02ef8974c5850914d
Andrew <andrewjuang01@gmail.com>
2021-03-09 22:04:13 -0500

    Implement partition

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -1,12 +1,35 @@
+import java.util.*;
+import java.io.*;
+
 public class Preliminary {
-    /*Modify the array such that:
-       *1. A random index from start to end inclusive is chosen, the corresponding
-       *   element is designated the pivot element.
-       *2. all elements in range that are smaller than the pivot element are placed before the pivot element.
-       *3. all elements in range that are larger than the pivot element are placed after the pivot element.
-       *4. Only the indices from start to end inclusive are considered in range
-       *@return the index of the final position of the pivot element.
-       */
-      public static int partition ( int [] data, int start, int end){
+      public static int partition (int[] data, int start, int end){
+          //find random pivot point
+          Random rand = new Random();
+          int randind = rand.nextInt((end-start)+1)+start;
+          int pivot = data[randind];
+          System.out.println(pivot);
+          //swap pivot to front
+          int temp = data[start];
+          data[start] = data[randind];
+          data[randind] = temp;
+
+          int i = end,j=start+1;
+          while(j<=i){
+              if(data[j]>pivot){
+                  temp = data[i];
+                  data[i] = data[j];
+                  data[j] = temp;
+                  i--;
+              }
+              if(data[j]<pivot){
+                  j++;
+              }
+          }
+          //move pivot back
+          temp = data[i];
+          data[i] = data[start];
+          data[start] = temp;
+          //return
+          return i;
       }
 }
