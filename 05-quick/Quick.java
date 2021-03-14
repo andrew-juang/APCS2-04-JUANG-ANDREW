5ef5c273ef1b7b19f92b3fcd07277434db0a7ffb
Andrew <andrewjuang01@gmail.com>
2021-03-14 17:31:28 -0400

    Implement dutchpartition

--- 05-quick/Quick.java
+++ 05-quick/Quick.java
@@ -2,6 +2,70 @@ import java.util.*;
 import java.io.*;
 
 public class Quick {
+    public static int quickselect(int[] data, int k){
+        return quickselect(data,0,data.length-1,k);
+    }
+
+    public static int quickselect(int[]data, int lo, int hi, int k){
+        int partition = partition(data,lo,hi);
+        if(partition == k){
+            return data[partition];
+        } else if (partition < k){
+            return quickselect(data,partition+1,hi,k);
+        } else {
+            return quickselect(data,lo,partition-1,k);
+        }
+    }
+
+    public static int[] partitionDutch(int[]data,int lo, int hi){
+        Random rand = new Random();
+        int randind = rand.nextInt((hi-lo)+1)+lo;
+        int pivot = data[randind];
+        int temp = data[hi];
+        data[hi] = data[randind];
+        data[randind] = temp;
+        int mid = lo;
+        int[]indices = new int[2];
+        while(mid<=hi){
+            if(data[mid]<pivot){
+                temp = data[lo];
+                data[lo]=data[mid];
+                data[mid]=temp;
+                lo++; mid++;
+            } else if(data[mid]>pivot){
+                temp=data[mid];
+                data[mid]=data[hi];
+                data[hi]=temp;
+                hi--;
+            } else {
+                mid++;
+            }
+            indices[0]=lo-1; indices[1]=mid;
+        }
+        return indices;
+    }
+
+    public static void quicksort(int[]data){
+        quicksort(data,0,data.length-1);
+    }
+
+    public static void quicksort(int[]data,int lo, int hi){
+        if(hi-lo==1){
+            if (data[lo] > data[hi]) {
+                int temp=data[lo];
+                data[lo]=data[hi];
+                data[hi]=temp;
+            }
+            return;
+        }
+        if(lo<hi){
+            int[]indices = new int[2];
+            indices = partitionDutch(data,lo,hi);
+            quicksort(data,lo,indices[0]);
+            quicksort(data,indices[1],hi);
+        }
+    }
+
     public static int partition(int[] data ,int lo, int hi){
         Random rand = new Random();
         int randind = rand.nextInt((hi-lo)+1)+lo;
@@ -43,36 +107,11 @@ public class Quick {
         return 0;
     }
 
-    /*return the value that is the kth smallest value of the array.
-    *@param data must have a length > 0
-    *@param k is 0 to data.length-1 inclusive
-    *@postcondition The array can be modified. (If we did not want to modify the array, we could make a copy before we start the process)
-    */
-    public static int quickselect(int[] data, int k){
-        return quickselect(data,0,data.length-1,k);
-    }
-
-    public static int quickselect(int[]data, int lo, int hi, int k){
-        int partition = partition(data,lo,hi);
-        if(partition == k){
-            return data[partition];
-        } else if (partition < k){
-            return quickselect(data,partition+1,hi,k);
-        } else {
-            return quickselect(data,lo,partition-1,k);
-        }
-    }
-
-    /*public static int[] partitionDutch(int[]data,int lo, int hi){
-        //THIS IS OPTIONAL METHOD, it will make your quicksort faster.
-        //return an int array: {lt,gt}
-    }*/
-
-    public static void quicksort(int[]data){
+    public static void regquicksort(int[]data){
         quicksort(data,0,data.length-1);
     }
 
-    public static void quicksort(int[]data,int lo, int hi){
+    public static void regquicksort(int[]data,int lo, int hi){
         if(lo<hi){
             int partition = partition(data,lo,hi);
             quicksort(data,lo,partition-1);
