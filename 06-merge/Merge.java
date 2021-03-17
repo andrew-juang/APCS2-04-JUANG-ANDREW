e22e09325b0b7cdb7aefcda34378f94efbe492d3
Andrew <andrewjuang01@gmail.com>
2021-03-17 17:20:11 -0400

    Improved speed

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -4,42 +4,41 @@ import java.io.*;
 public class Merge {
 
     public static void mergesort (int[]data) {
-        int temp[] = new int[data.length-1];
+        int temp[] = new int[data.length];
+        for(int i=0;i<data.length;i++){
+            temp[i]=data[i];
+        }
         mergesort(data,temp,0,data.length-1);
     }
 
     public static void mergesort (int[]data,int[]temp, int lo, int hi) {
         int mid = lo + (hi-lo)/2;
         if(lo<hi){
-            mergesort(data,temp,lo,mid);
-            mergesort(data,temp,mid+1,hi);
-            merge(data,lo,hi);
+            mergesort(temp,data,lo,mid);
+            mergesort(temp,data,mid+1,hi);
+            merge(data,temp,lo,hi);
         }
     }
 
-    private static void merge(int[]data, int lo, int hi){
+    private static void merge(int[]data, int[]temp, int lo, int hi){
         //i,j iterate through the two halves, ind keeps track on index in temp arr
-        int mid = lo + (hi-lo)/2,i=lo,j=mid+1,ind=0;
-        int temp[] = new int[hi-lo+1];
+        int mid = lo + (hi-lo)/2,i=lo,j=mid+1,ind=lo;
         while(i<=mid&&j<=hi){
-            if(data[i]<=data[j]){
-                temp[ind]=data[i];
+            if(temp[i]<=temp[j]){
+                data[ind]=temp[i];
                 i++;ind++;
             } else {
-                temp[ind]=data[j];
+                data[ind]=temp[j];
                 j++;ind++;
             }
         }
         while(i<=mid){
-            temp[ind]=data[i];
+            data[ind]=temp[i];
             i++;ind++;
         }
         while(j<=hi){
-            temp[ind]=data[j];
+            data[ind]=temp[j];
             j++;ind++;
-        }
-        for(i=0;i<hi-lo+1;i++){
-            data[i+lo]=temp[i];
         }
     }
 }
