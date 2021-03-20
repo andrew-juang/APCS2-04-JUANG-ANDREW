943cc18feb2d4feff44d22ec8a817eda9404785c
Andrew <andrewjuang01@gmail.com>
2021-03-20 14:31:21 -0400

    Implement constructors

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -1,10 +1,27 @@
-public class MyDeque<E>{
+import java.util.*;
+import java.io.*;
+
+public class MyDeque<E> {
     private E[] data;
     private int size, start, end;
 
-    public MyDeque(){  }
-    public MyDeque(int initialCapacity){  }
-    public int size(){ }
+    @SupressWarnings("unchecked")
+    public MyDeque(){
+        start = -1;
+        end = 0;
+        size = 10;
+        data = (E[])new Object[10];
+    }
+
+    public MyDeque(int initialCapacity){
+        start = -1;
+        end = 0;
+        size = initialCapacity;
+        data = (E[])new Object[initialCapacity];
+    }
+    public int size(){
+        return data.size;
+    }
     public String toString(){ }
     public void addFirst(E element){ }
     public void addLast(E element){ }
