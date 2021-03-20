96f23e96a25c72f46b133091da57751b9e6e30e2
Andrew <andrewjuang01@gmail.com>
2021-03-20 14:57:21 -0400

    Implement toString

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -9,7 +9,7 @@ public class MyDeque<E> {
     public MyDeque(){
         start = -1;
         end = 0;
-        size = 10;
+        size = 0;
         data = (E[])new Object[10];
     }
 
@@ -21,11 +21,22 @@ public class MyDeque<E> {
     }
 
     public int size(){
-        return data.size;
+        return start-end+1;
     }
 
-    public String toString(){ }
-    public void addFirst(E element){ }
+    public String toString(){
+        if(isEmpty()) return "[]";
+        String result = "[";
+        result += data[start];
+        for(int i=(start+1)%size;i!=end;i=(i+1)%size) {
+            result += ", " + data[i];
+        }
+        return result + "]";
+
+    }
+    public void addFirst(E element){
+
+    }
     public void addLast(E element){ }
     public E removeFirst(){ }
     public E removeLast(){ }
