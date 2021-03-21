e1c03011b76614d03d7e8ee864052e2f478734cd
Andrew <andrewjuang01@gmail.com>
2021-03-21 11:59:28 -0400

    Implement addFirst

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -29,7 +29,7 @@ public class MyDeque<E> {
         if(isEmpty()) return "[]";
         String result = "[";
         result += data[start];
-        for(int i=(start+1)%size;i!=end;i=(i+1)%size) {
+        for(int i=(start+1)%data.length;i!=end+1;i=(i+1)%data.length) {
             result += ", " + data[i];
         }
         return result + "]";
@@ -41,8 +41,15 @@ public class MyDeque<E> {
             resize();
         }
         if(start==-1){
-            
+            start = 0;
+            end = 0;
+        } else if (start==0){
+            start = data.length-1;
+        } else {
+            start--;
         }
+        data[start] = element;
+        size++;
     }
 
     /*
@@ -53,20 +60,22 @@ public class MyDeque<E> {
     public E getLast(){ }*/
 
     private boolean isFull(){
-        return ((end==size-1&&start==0)||start==end+1);
+        return ((end==data.length-1&&start==0)||start==end+1);
     }
 
     private boolean isEmpty(){
         return start==-1;
     }
 
+    @SuppressWarnings("unchecked")
     private void resize(){
-        E[] newarr = (E[]) new Object[2*size];
+        E[] newarr = (E[]) new Object[2*data.length];
         int ind = 0;
-        for(int i=(start)%size;i!=end;i=(i+1)%size) {
+        for(int i=(start)%data.length;i!=end;i=(i+1)%data.length) {
             newarr[ind] = data[i];
             ind++;
         }
+        newarr[ind] = data[end];
         start = 0;
         end = data.length-1;
         data = newarr;
