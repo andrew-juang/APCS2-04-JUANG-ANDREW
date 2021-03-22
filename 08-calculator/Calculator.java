33e7a423d8ea878fcf08f5402becd7c9ccc7067b
Andrew <andrewjuang01@gmail.com>
2021-03-22 14:44:19 -0400

    Implement eval()

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -9,5 +9,25 @@ public class Calculator {
     *@throws IllegalArgumentException when there are too many or too few operands.
     *        Use the string parameter of your exception to indicate what happened.
     */
-    public static double eval(String s){}
+    public static double eval(String s){
+        String[] chars = s.split(" ");
+        int digitcount = 0;
+        for(int i=0;i<chars.length;i++){
+            if(Character.isDigit(chars[i].charAt(0))) digitcount++;
+        }
+        if(digitcount*2-1!=chars.length) throw new IllegalArgumentException();
+        ArrayDeque<Double> deque = new ArrayDeque<Double>();
+        for(int i=0;i<chars.length;i++){
+            if(Character.isDigit(chars[i].charAt(0))){
+                deque.addLast(Double.parseDouble(chars[i]));
+            } else {
+                double val2=deque.removeLast(), val1=deque.removeLast();
+                if (chars[i].equals("+")) deque.addLast(val1+val2);
+                if (chars[i].equals("-")) deque.addLast(val1-val2);
+                if (chars[i].equals("/")) deque.addLast(val1/val2);
+                if (chars[i].equals("*")) deque.addLast(val1*val2);
+            }
+        }
+        return deque.getLast();
+    }
 }
