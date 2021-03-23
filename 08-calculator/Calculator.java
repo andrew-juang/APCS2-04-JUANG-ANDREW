83de6dc7bb3b729b04fc84ecf889bdcaa583a787
Andrew <andrewjuang01@gmail.com>
2021-03-23 13:52:36 -0400

    Cleaned up code

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -2,32 +2,23 @@ import java.util.*;
 import java.io.*;
 
 public class Calculator {
-    /*Evaluate a postfix expression stored in s.
-    *Assume valid postfix notation, of ints doubles and operators separated by spaces.
-    *Valid operators are + - / * and % (remainder not modulo)
-    *All results are doubles even if the operands are both int.
-    *@throws IllegalArgumentException when there are too many or too few operands.
-    *        Use the string parameter of your exception to indicate what happened.
-    */
     public static double eval(String s){
         String[] chars = s.split(" ");
-        int digitcount = 0;
-        for(int i=0;i<chars.length;i++){
-            if(Character.isDigit(chars[i].charAt(0))) digitcount++;
-        }
-        if(digitcount*2-1!=chars.length) throw new IllegalArgumentException();
         ArrayDeque<Double> deque = new ArrayDeque<Double>();
         for(int i=0;i<chars.length;i++){
-            if(Character.isDigit(chars[i].charAt(0))){
-                deque.addLast(Double.parseDouble(chars[i]));
-            } else {
+            if("+-*/%".contains(chars[i])){
+                if(deque.size()<=1)throw new IllegalArgumentException("Too few operands");
                 double val2=deque.removeLast(), val1=deque.removeLast();
                 if (chars[i].equals("+")) deque.addLast(val1+val2);
                 if (chars[i].equals("-")) deque.addLast(val1-val2);
                 if (chars[i].equals("/")) deque.addLast(val1/val2);
                 if (chars[i].equals("*")) deque.addLast(val1*val2);
+                if (chars[i].equals("%")) deque.addLast(val1%val2);
+            } else {
+                deque.addLast(Double.parseDouble(chars[i]));
             }
         }
+        if(deque.size()>1)throw new IllegalArgumentException("Too many operands");
         return deque.getLast();
     }
 }
