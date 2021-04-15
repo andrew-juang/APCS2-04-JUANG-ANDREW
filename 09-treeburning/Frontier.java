2cd18bc5a277fda1c94ecdada174ee0460f664dd
Andrew <andrewjuang01@gmail.com>
2021-04-15 13:41:56 -0400

    Removed static

--- 09-treeburning/Frontier.java
+++ 09-treeburning/Frontier.java
@@ -2,21 +2,21 @@ import java.util.*;
 import java.io.*;
 
 public class Frontier {
-    private static LinkedList<int[]> frontier;
+    private LinkedList<int[]> frontier;
 
     public Frontier(){
         frontier = new LinkedList<int[]>();
     }
 
-    public static int size(){
+    public int size(){
         return frontier.size();
     }
 
-    public static void add (int[]location){
+    public void add (int[]location){
         frontier.addFirst(location);
     }
 
-    public static int[] remove(){
+    public int[] remove(){
         return (frontier.removeLast());
     }
 }
