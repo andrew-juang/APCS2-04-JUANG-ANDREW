3afbc6091ad15bd2ecf0c034e935f72d5838bc3e
Andrew <andrewjuang01@gmail.com>
2021-04-23 16:06:15 -0400

    IMplement remove

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class MyHeap {
-    private static void pushDown(int[]data, int size, int index) {
+    public static void pushDown(int[]data, int size, int index) {
         int l = 2*index+1;
         int r = 2*index+2;
         int big = index;
@@ -20,9 +20,27 @@ public class MyHeap {
         }
     }
 
-    private static void buildHeap(int[]data) {
-        for(int i=(data.size/2)-1; i>=0; i--){
-            pushDown(data,data.size,i);
+    public static void buildHeap(int[]data) {
+        for(int i=(data.length/2)-1; i>=0; i--){
+            pushDown(data,data.length,i);
         }
     }
+
+    /*Swap the root node with the element at the provided index.
+    *Then push the new root down, but not past the index that it swapped with.
+    *@precondition: size is between 0 and data.length inclusive.
+    */
+    private static void remove(int[]data,int size){
+        int swap = data[0];
+        data[0] = data[data.length-1];
+        data[data.length-1] = swap;
+        for(int i=((data.length-1)/2)-1; i>=0; i--){
+            pushDown(data,data.length-1,i);
+        }
+    }
+
+    /*Sort the provided array
+    *@param data is the array to be sorted
+    */
+    public static void heapsort(int[]data)//We will discuss this next time!:
 }
