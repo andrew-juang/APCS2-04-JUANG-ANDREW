72b76a0c25accec7a39a6939f62e3c7bea931dcb
Andrew <andrewjuang01@gmail.com>
2021-04-23 15:50:21 -0400

    Implement buildHeap

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -20,8 +20,9 @@ public class MyHeap {
         }
     }
 
-    /*Reorder the provided array to be a valid heap.
-    *@param data is the array to be modified
-    */
-    private static void buildHeap(int[]data)//We will discuss this today:
+    private static void buildHeap(int[]data) {
+        for(int i=(data.size/2)-1; i>=0; i--){
+            pushDown(data,data.size,i);
+        }
+    }
 }
